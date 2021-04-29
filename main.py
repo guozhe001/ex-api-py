79bee9a6aad3c4a898b6a3001c8477412d77ad8a
guozhe <guozhe@renmaitech.com>
2021-04-29 15:03:27 +0800

    添加binance的sdk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # encoding=utf8
 
+from binance.client import Client
+
 from config import config_util
 from constant import constant
 
@@ -7,3 +9,7 @@ from constant import constant
 if __name__ == '__main__':
     key, secret = config_util.get_api_key_secret(exchange=constant.EXCHANGE_BINANCE)
     print("key={}, secret={}".format(key, secret))
+    client = Client(key, secret)
+    depth = client.get_order_book(symbol='BNBBTC')
+    print(type(depth))
+    print(depth)
